1142f8e234cd0b55008957ccb31a81e9ee7755aa
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2026-04-05 15:51:44 +0200

    Initial commit

---
+++ src/main/java/com/zim4ik/restaurantvoting/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package com.zim4ik.restaurantvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+}

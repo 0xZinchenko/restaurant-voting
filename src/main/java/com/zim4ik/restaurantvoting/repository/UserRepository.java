c201d0ee8b45575939ccab5c6469f37094a628a9
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2026-04-19 13:18:22 +0200

    feat: add Spring Data REST and user search endpoints

--- src/main/java/com/zim4ik/restaurantvoting/repository/UserRepository.java
+++ src/main/java/com/zim4ik/restaurantvoting/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.zim4ik.restaurantvoting.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email) ")
     Optional<User> findByEmailIgnoreCase(String email);
 
+    @RestResource(rel = "by-lastname", path = "by-lastname")
     List<User> findByLastNameContainingIgnoreCase(String lastName);
 }
